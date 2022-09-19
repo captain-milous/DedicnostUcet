549e073612483561aabed3efb594f92d4b42c0ba
captain-milous <milda.tesar3@gmail.com>
2022-09-19 08:43:54 +0200

    Add project files.

---
+++ DedicnostUcet/Penize.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DedicnostUcet
+{
+    internal class Penize
+    {
+        private double castka;
+        private string mena; // CZK, EUR, USD
+
+        public Penize(double castka, string mena)
+        {
+            if(castka < 0)
+            {
+                throw new ArgumentException("Penize nemohou byt zaporne");
+            } else
+            this.castka = castka;
+            this.mena = mena;
+        }
+
+        public Penize()
+        {
+            this.castka = 0;
+            this.mena = "CZK";
+        }
+
+        public double Castka
+        {
+            get { return this.castka; }
+            set {
+                if (castka < 0)
+                {
+                    throw new ArgumentException("Penize nemohou byt zaporne");
+                } else
+                    this.castka = value; 
+            }
+        }
+        public string Mena
+        {
+            get { return this.mena; }
+            set { this.mena = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.castka + " " + this.mena;
+        }
+
+        public virtual Penize toCZK()
+        {
+            if (this.mena == "EUR")
+            {
+                return new Penize(this.castka * 24.52, "CZK");
+            }
+            if (this.mena == "USD")
+            {
+                return new Penize(this.castka * 24.56, "CZK");
+            }
+            else return new Penize(this.castka, "CZK");
+        }
+
+        public virtual Penize toEUR()
+        {
+            if (this.mena == "CZK")
+            {
+                return new Penize(this.castka * 0.041, "EUR");
+            }
+            if (this.mena == "USD")
+            {
+                return new Penize(this.castka * 1, "EUR");
+            }
+            else return new Penize(this.castka, "EUR");
+        }
+
+        public virtual Penize toUSD()
+        {
+            if (this.mena == "EUR")
+            {
+                return new Penize(this.castka * 1, "USD");
+            }
+            if (this.mena == "CZK")
+            {
+                return new Penize(this.castka * 0.041, "USD");
+            }
+            else return new Penize(this.castka, "USD");
+        }
+    }
+}
---
+++ DedicnostUcet/Ucet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DedicnostUcet
+{
+    internal class Ucet
+    {
+        protected Penize stav;
+
+        public Ucet()
+        {
+            this.stav = new Penize();
+        }
+
+        public Ucet(double stav, string mena)
+        {
+            this.stav = new Penize(stav, mena);
+        }
+
+        public void Vyber(Penize castka)
+        {
+
+        }
+    }
+}
